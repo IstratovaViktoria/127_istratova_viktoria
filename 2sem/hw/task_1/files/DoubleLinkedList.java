61ee887732b267ffd3b38dde456a780ba6347a5f
IstratovaViktoria <vikanika19.02@mail.ru>
2022-03-29 00:42:13 +0300

    дописала тесты и написала отчеты

--- 2sem/hw/task_1/files/DoubleLinkedList.java
+++ 2sem/hw/task_1/files/DoubleLinkedList.java
@@ -14,18 +14,18 @@ public class DoubleLinkedList<T> {
         return size;
     }
     public ListNode<T> getHead() {
-        return dummy.getPrev();
+        return dummy.getNext();
     }
     public ListNode<T> getTail() {
-        return dummy.getNext();
+        return dummy.getPrev();
     }
 
     public ListNode<T> get(int index) {
-        if(index < 0 || index > size)
+        if(index < 0 || index >= size)
             throw new IndexOutOfBoundsException("index out of bounds");
         ListNode<T> cur = getHead();
         for(int i = 0; i < index; ++i)
-            cur = cur.getPrev();
+            cur = cur.getNext();
         return (Node<T>) cur;
     }
 
@@ -38,10 +38,10 @@ public class DoubleLinkedList<T> {
         return new Node<T>(data, node, node.getPrev());
     }
     public Node<T> pushFront(T data) {
-        return insertBefore(getTail(), data);
+        return insertAfter(getTail(), data);
     }
     public Node<T> pushBack(T data) {
-        return insertAfter(getHead(), data);
+        return insertBefore(getHead(), data);
     }
     public void remove(ListNode<T> node) {
         node.getPrev().setNext(node.getNext());
@@ -52,7 +52,7 @@ public class DoubleLinkedList<T> {
     public void insertListAfter(ListNode<T> node, DoubleLinkedList<T> list) {
         if(list.isEmpty())
             return;
-        list.getHead().setNext(node);
+        list.getHead().setPrev(node);
         list.getTail().setNext(node.getNext());
         node.getNext().setPrev(list.getTail());
         node.setNext(list.getHead());
@@ -64,12 +64,13 @@ public class DoubleLinkedList<T> {
     public void insertListBefore(ListNode<T> node, DoubleLinkedList<T> list) {
         if(list.isEmpty())
             return;
-        list.getHead().setNext(node.getPrev());
-        list.getTail().setPrev(node);
+        list.getHead().setPrev(node.getPrev());
+        list.getTail().setNext(node);
         node.getPrev().setNext(list.getHead());
         node.setPrev(list.getTail());
 
         list.dummy.setNext(list.dummy);
         list.dummy.setPrev(list.dummy);
+        size += list.getSize();
     }
 }
--- 2sem/hw/task_1/files/Task1_Tests.java
+++ 2sem/hw/task_1/files/Task1_Tests.java
@@ -6,7 +6,7 @@ public class Task1_Tests extends Assert{
     assertTrue(list.isEmpty());
 }
     @Test
-    public void create_DoubleLinkedList_EmptyFalse(){
+    public void create_EmptyFalse(){
         DoubleLinkedList<Integer> list = new DoubleLinkedList<>();
         list.pushBack(1);
         assertFalse(list.isEmpty());
@@ -76,7 +76,7 @@ public class Task1_Tests extends Assert{
         assertEquals(1, num);
     }
     @Test
-    public void get_PushBackEmptyDoubleLinkedList_CheckFirst(){
+    public void get_CheckFirst(){
         DoubleLinkedList<Integer> list = new DoubleLinkedList<>();
         list.pushBack(3);
         list.pushBack(2);
@@ -123,7 +123,7 @@ public class Task1_Tests extends Assert{
         assertEquals(1, head);
     }
     @Test
-    public void insertListAfter_CreateTwoListsAndUnite_CheckValues() {
+    public void insertListAfter_CheckValues() {
         DoubleLinkedList<Integer> list1 = new DoubleLinkedList<>();
         list1.pushFront(1);
         list1.pushFront(2);
@@ -169,5 +169,216 @@ public class Task1_Tests extends Assert{
         list.remove(list.get(0));
         int num = ((Node<Integer>)list.get(0)).getData();
         assertEquals(2, num);
+    }
+    @Test
+    public void insertListBefore_CheckSize(){
+        DoubleLinkedList<Integer> list = new DoubleLinkedList<>();
+        list.pushFront(4);
+        list.pushFront(5);
+        list.pushFront(6);
+        DoubleLinkedList<Integer> list1 = new DoubleLinkedList<>();
+        list1.pushFront(1);
+        list1.pushFront(2);
+        list1.pushFront(3);
+        list.insertListBefore(list.get(0), list1);
+        assertEquals(6, list.getSize());
+    }
+    @Test
+    public void insertListBefore_CheckValues(){
+        DoubleLinkedList<Integer> list = new DoubleLinkedList<>();
+        list.pushFront(4);
+        list.pushFront(5);
+        list.pushFront(6);
+        DoubleLinkedList<Integer> list1 = new DoubleLinkedList<>();
+        list1.pushFront(1);
+        list1.pushFront(2);
+        list1.pushFront(3);
+        list.insertListBefore(list.get(0), list1);
+        int num1 = ((Node<Integer>)list.get(0)).getData();
+        assertEquals(1, num1);
+        num1 = ((Node<Integer>)list.get(1)).getData();
+        assertEquals(2, num1);
+        num1 = ((Node<Integer>)list.get(2)).getData();
+        assertEquals(3, num1);
+        num1 = ((Node<Integer>)list.get(3)).getData();
+        assertEquals(4, num1);
+        num1 = ((Node<Integer>)list.get(4)).getData();
+        assertEquals(5, num1);
+        num1 = ((Node<Integer>)list.get(5)).getData();
+        assertEquals(6, num1);
+    }
+    @Test
+    public void createArray_CheckSize1(){
+        DynamicArray<Integer> array = new DynamicArray<>(5);
+        assertEquals(5, array.getSize());
+    }
+    @Test
+    public void createArray_CheckSize2(){
+        DynamicArray<Integer> array = new DynamicArray<>();
+        assertEquals(1024, array.getSize());
+    }
+    @Test
+    public void resize_CheckSize(){
+        DynamicArray<Integer> array = new DynamicArray<>(5);
+        array.resize(10);
+        assertEquals(10, array.getSize());
+    }
+    @Test
+    public void get_CheckException2(){
+        DynamicArray<Integer> array = new DynamicArray<>(5);
+        try {
+            array.get(7);
+            fail();
+        }
+        catch (IndexOutOfBoundsException some) {
+            assertEquals("Index out of bounds", some.getMessage());
+        }
+    }
+    @Test
+    public void get_CheckException(){
+        DynamicArray<Integer> array = new DynamicArray<>(5);
+        try {
+            array.get(5);
+            fail();
+        }
+        catch (IndexOutOfBoundsException some) {
+            assertEquals("Index out of bounds", some.getMessage());
+        }
+    }
+    @Test
+    public void set_CheckExceptio2(){
+        DynamicArray<Integer> array = new DynamicArray<>(5);
+        try {
+            array.set(6,10);
+            fail();
+        }
+        catch (IndexOutOfBoundsException some) {
+            assertEquals("Index out of bounds", some.getMessage());
+        }
+    }
+    @Test
+    public void set_CheckException(){
+        DynamicArray<Integer> array = new DynamicArray<>(5);
+        try {
+            array.set(5,10);
+            fail();
+        }
+        catch (IndexOutOfBoundsException some) {
+            assertEquals("Index out of bounds", some.getMessage());
+        }
+    }
+    @Test
+    public void insert_CheckException2(){
+        DynamicArray<Integer> array = new DynamicArray<>(5);
+        try {
+            array.insert(6,10);
+            fail();
+        }
+        catch (IndexOutOfBoundsException some) {
+            assertEquals("Index out of bounds", some.getMessage());
+        }
+    }
+    @Test
+    public void insert_CheckException(){
+        DynamicArray<Integer> array = new DynamicArray<>(5);
+        try {
+            array.insert(5,10);
+            fail();
+        }
+        catch (IndexOutOfBoundsException some) {
+            assertEquals("Index out of bounds", some.getMessage());
+        }
+    }
+    @Test
+    public void set_CheckValues(){
+        DynamicArray<Integer> array = new DynamicArray<>(5);
+        array.set(0, 1);
+        array.set(1, 2);
+        array.set(2, 3);
+        array.set(3, 4);
+        array.set(4, 5);
+        assertEquals(1, array.get(0).intValue());
+        assertEquals(2, array.get(1).intValue());
+        assertEquals(3, array.get(2).intValue());
+        assertEquals(4, array.get(3).intValue());
+        assertEquals(5, array.get(4).intValue());
+    }
+    @Test
+    public void insert_CheckNewValue(){
+        DynamicArray<Integer> array = new DynamicArray<>(5);
+        array.set(0, 1);
+        array.set(1, 2);
+        array.set(2, 3);
+        array.set(3, 4);
+        array.set(4, 5);
+        array.insert(4, 100);
+        assertEquals(1, array.get(0).intValue());
+        assertEquals(2, array.get(1).intValue());
+        assertEquals(3, array.get(2).intValue());
+        assertEquals(4, array.get(3).intValue());
+        assertEquals(100, array.get(4).intValue());
+    }
+    @Test
+    public void pushBack_CheckValues(){
+        DynamicArray<Integer> array = new DynamicArray<>(5);
+        array.set(0, 1);
+        array.set(1, 2);
+        array.set(2, 3);
+        array.set(3, 4);
+        array.set(4, 5);
+        array.pushBack(6);
+        array.pushBack(7);
+        assertEquals(1, array.get(0).intValue());
+        assertEquals(2, array.get(1).intValue());
+        assertEquals(3, array.get(2).intValue());
+        assertEquals(4, array.get(3).intValue());
+        assertEquals(5, array.get(4).intValue());
+        assertEquals(6, array.get(5).intValue());
+        assertEquals(7, array.get(6).intValue());
+    }
+    @Test
+    public void popBack_CheckException(){
+        DynamicArray<Integer> array = new DynamicArray<>(0);
+        try {
+            array.popBack();
+            fail();
+        }
+        catch (Exception some) {
+            assertEquals("Array is empty", some.getMessage());
+        }
+    }
+    @Test
+    public void popBack_CheckCountArray(){
+        DynamicArray<Integer> array = new DynamicArray<>(5);
+        array.popBack();
+        assertEquals(4, array.getSize());
+    }
+    @Test
+    public void remove_CheckException2(){
+        DynamicArray<Integer> array = new DynamicArray<>(5);
+        try {
+            array.remove(6);
+            fail();
+        }
+        catch (Exception some) {
+            assertEquals("Index out of bounds", some.getMessage());
+        }
+    }
+    @Test
+    public void remove_CheckException(){
+        DynamicArray<Integer> array = new DynamicArray<>(5);
+        try {
+            array.remove(5);
+            fail();
+        }
+        catch (Exception some) {
+            assertEquals("Index out of bounds", some.getMessage());
+        }
+    }
+    @Test
+    public void remove_CheckSize(){
+        DynamicArray<Integer> array = new DynamicArray<>(5);
+        array.remove(3);
+        assertEquals(4, array.getSize());
     }
 }
